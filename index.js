007840d8e8f71d4b0df36ded096a0b178c5605c9
Iliya Hakani <iliahakani@gmail.com>
2020-05-12 17:51:54 +0400

    second commit

--- index.js
+++ index.js
@@ -8,3 +8,66 @@ express()
     //.set('view engine', 'ejs')
     .get("/", (req, res) => res.send("./index.html"))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const port = 3000;
+const app = express();
+const http = require('http').Server(app);
+
+const io = require('socket.io')(http);
+
+const db = require('./.dbConfigs');
+
+const connectionUrl = `mongodb+srv://${db.username}:${db.password}@${db.dbUri}`;
+
+mongoose.connect(connectionUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('DB connection successful');
+});
+
+const MessageStructure = mongoose.model('messages', {
+    name: String,
+    text: String,
+});
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.get('/messageEndpoint', (request, response) => {
+    MessageStructure.find({}, (err, allMessages) => {
+        if (err) {
+            response.sendStatus(500);
+        }
+        response.send(allMessages);
+    });
+});
+
+app.post('/messageEndpoint', (request, response) => {
+    const messageObject = new MessageStructure(request.body);
+    messageObject.save((err) => {
+        if (err) {
+            response.sendStatus(500);
+        }
+        io.emit('messageIncome', request.body);
+        response.sendStatus(200);
+    });
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+http.listen(port, () => {
+    console.log(`server is running on the port ${port}`);
+});
